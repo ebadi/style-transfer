8152018c4e1af5f7d5398559d5c3003093478194
Hamid Ebadi <hamid.ebadi@gmail.com>
2026-05-25 15:52:42 +0200

    fix: measuring inference time

--- infer.sh
+++ infer.sh
@@ -5,7 +5,7 @@ cd "$(dirname "$0")"
 
 START=$(date +%s)
 
-docker compose --profile inference up
+docker compose --profile inference up --abort-on-container-exit --exit-code-from worker
 
 END=$(date +%s)
 ELAPSED=$(( END - START ))
